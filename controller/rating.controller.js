862ef8e0cccce7c5ce2098848189787770a501c5
trjeremiahcelis <59946355+canlascelis@users.noreply.github.com>
2022-08-25 09:35:01 +0800

    Added reset rating endpoint

--- controller/rating.controller.js
+++ controller/rating.controller.js
@@ -8,10 +8,15 @@ const saveRating = app.post('/', authenticateToken, async (req, res) => {
     const { rate_count, user_name_rating } = req.body;
 
     try {
-        await client.query('INSERT INTO rating (rate_count, user_name_rating) VALUES($1, $2) RETURNING *',
-            [rate_count, user_name_rating]);
-        res.json({ message: 'Successfully rated!' });
-        res.status(200);
+        if (!rate_count === null && user_name_rating === null) {
+            res.json({ message: 'You must input value' })
+            res.status(400)
+        } else {
+            await client.query('INSERT INTO rating (rate_count, user_name_rating) VALUES($1, $2) RETURNING *',
+                [rate_count, user_name_rating]);
+            res.json({ message: 'Successfully rated!' });
+            res.status(200);
+        }
     } catch (error) {
         res.json({ message: 'Error occured!' });
         res.status(400);
@@ -26,7 +31,7 @@ const getRatingResults = app.get('/', authenticateToken, async (req, res) => {
                 res.json(result.rows);
                 res.status(200);
             } else {
-                res.json({message: 'Error'});
+                res.json({ message: 'Error query' });
                 res.status(401)
             }
         })
@@ -37,4 +42,24 @@ const getRatingResults = app.get('/', authenticateToken, async (req, res) => {
     }
 })
 
-export default { saveRating, getRatingResults };
+const resetRating = app.delete('/reset-ratings', authenticateToken, async (req, res) => {
+    try {
+        await client.query('TRUNCATE rating RESTART IDENTITY', (err, result) => {
+            if(!err) {
+                res.json({message: 'Successfully reset your portfolio ratings'})
+                res.status(200)
+                console.log(result)
+            } else {
+                res.json({message: 'Error resetting your portfolio rating results'})
+                console.log(err);
+            }
+        })
+    } catch (error) {
+        res.json({message: 'Error while resetting your portfolio results'});
+        res.status(400);
+        client.end();
+        throw error
+    }
+})
+
+export default { saveRating, getRatingResults, resetRating };
--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -4,6 +4,7 @@ import ratingController from "../controller/rating.controller.js";
 const app = express.Router()
 
 app.use('/rating', ratingController.saveRating);
-app.use('/rating', ratingController.getRatingResults)
+app.use('/rating', ratingController.getRatingResults);
+app.use('/rating', ratingController.resetRating);
 
 export default app
